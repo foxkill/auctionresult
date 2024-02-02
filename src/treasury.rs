a615cc1aedbf195d0fd024709de7af36194afd17
martinpfh <martin@pfh.de>
2024-02-02 19:04:21 +0100

    Flag all field with the correct attributes for deserializing.

--- src/treasury.rs
+++ src/treasury.rs
@@ -1,73 +1,43 @@
 //! # Treasury
 //! 
 //! 
-use serde::{de::{self, Unexpected}, Deserialize, Deserializer};
-use serde_json::Value;
+use serde::{Deserialize};
+// use serde_with::{serde_as, DisplayFromStr};
 
+mod deserializer;
 mod treasury_type;
 
-/// Deserialize bool from String with custom value mapping
-fn bool_from_string<'de, D>(deserializer: D) -> Result<bool, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    match String::deserialize(deserializer)?.as_ref() {
-        "Yes" => Ok(true),
-        "No" => Ok(false),
-        other => Err(de::Error::invalid_value(
-            Unexpected::Str(other),
-            &"Yes or No",
-        )),
-    }
-}
+use deserializer::f64_from_string;
+use deserializer::bool_from_string;
+use treasury_type::TreasuryType;
 
-fn de_f64_or_string_as_f64<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Option<f64>, D::Error> {
-  Ok(match Value::deserialize(deserializer)? {
-    Value::String(s) => s.parse().ok(),
-    Value::Number(num) => num.as_f64(),
-    _ => None,
-  })
-}
-// fn de_f64_or_string_as_f64<'de, D: Deserializer<'de>>(deserializer: D) -> Result<f64, D::Error> {
-//   Ok(match Value::deserialize(deserializer)? {
-//     Value::String(s) => s.parse().map_err(de::Error::custom)?,
-//     Value::Number(num) => num.as_f64().ok_or_else(|| de::Error::custom("Invalid number"))?,
-//     _ => return Err(de::Error::custom("wrong type")),
-//   })
-// }
 #[allow(dead_code)]
 #[derive(Debug, Default, Deserialize)]
-pub struct Treasury<'a> {
-    cusip: &'a str,
+#[serde(rename_all = "camelCase")]
+pub struct Treasury {
+    cusip: String,
     #[serde(rename(deserialize = "type"))]
-    treasury_type: &'a str,
-    term: &'a str,
-    #[serde(rename="securityTerm")]
-    security_term : &'a str,
+    treasury_type: TreasuryType,
+    term: String,
+    security_term : String,
     #[serde(deserialize_with = "bool_from_string")]
     reopening: bool,
-    #[serde(rename="issueDate")]
-    issue_date: &'a str,
-    #[serde(rename="maturityDate")]
-    maturity_date: &'a str,
-    #[serde(rename="highYield")]
+    issue_date: String,
+    maturity_date: String,
+    #[serde(deserialize_with = "f64_from_string")]
     high_yield: f64,
-    #[serde(rename="interestRate")]
+    #[serde(deserialize_with = "f64_from_string")]
     interest_rate: f64,
-    #[serde(rename="highDiscountRate")]
+    #[serde(deserialize_with = "f64_from_string")]
     high_discount_rate: f64,
-    #[serde(rename="highInvestmentRate")]
+    #[serde(deserialize_with = "f64_from_string")]
     high_investment_rate:  f64,
-    #[serde(rename="asResults")]
-    as_results: bool,
-    #[serde(rename="mlFileNameCompetitiveResult")]
-    ml_filename_competitive_results: &'a str,
-    #[serde(rename="primaryDealerAccepted")]
-    primary_dealer_dccepted: f64,
-    #[serde(rename="bidToCoverRatio")]
-    bid_to_cover_ratio: f32,
-    #[serde(rename="totalAccepted")]
-    total_accepted: f32,
+    #[serde(deserialize_with = "f64_from_string")]
+    primary_dealer_accepted: f64,
+    #[serde(deserialize_with = "f64_from_string")]
+    bid_to_cover_ratio: f64,
+    #[serde(deserialize_with = "f64_from_string")]
+    total_accepted: f64,
 }
 
 #[cfg(test)]
