0a58c5cdf4dad850db3ed74286fe5cb14c91e42e
martinpfh <martin@pfh.de>
2024-02-03 17:18:00 +0100

    Remove macro enum_to_string

--- src/macros.rs
+++ src/macros.rs
@@ -4,25 +4,6 @@
 //
 
 #[allow(unused)]
-macro_rules! enum_to_string {
-    (enum $name:ident {
-        $($variant:ident = $val:expr),*,
-    }) => {
-        enum $name {
-            $($variant = $val),*
-        }
-
-        impl $name {
-            fn name(&self) -> &'static str {
-                match self {
-                    $($name::$variant => stringify!($variant)),*
-                }
-            }
-        }
-    };
-}
-
-#[allow(unused)]
 macro_rules! hashmap {
     ($($key:expr => $value:expr),*) => ({
         let mut map = HashMap::new();
