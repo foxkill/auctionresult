caa0c6f300ed0311844fd40caf202a5bc4313514
martinpfh <martin@pfh.de>
2024-02-03 20:14:54 +0100

    Temporarily remove code from main binary

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use auctionresult::{Latest, Treasury, TreasuryType};
 
 
 fn main() {
-    let l = Latest::new(TreasuryType::Bill, 0);
-    let r: Vec<Treasury> = l.get();
+    // let l = Latest::new(TreasuryType::Bill, 0);
+    // let r: Vec<Treasury> = l.get();
 
-    println!("{:#?}", r);
+    // println!("{:#?}", r);
 }
