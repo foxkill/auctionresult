4e4a600511d39085b72f352ac2ac0f57fcbda7a8
martinpfh <martin@pfh.de>
2024-02-04 16:27:16 +0100

    Use blocking get() function to load data.

--- src/treasury/load.rs
+++ src/treasury/load.rs
@@ -3,17 +3,16 @@
 
 use std::thread::{self, JoinHandle};
 
-use reqwest::blocking::Client;
+use reqwest::blocking::get;
 use super::Treasury;
 
 /// Naive version of non blocking request get.
 pub fn load(url: impl Into<String>) -> JoinHandle<Vec<Treasury>> {
-    let client = Client::new();
     let def = vec![Treasury::default()];
     let url = url.into();
 
     thread::spawn(move || {
-        let Ok(resp) = client.get(url).send() else {
+        let Ok(resp) = get(url) else {
             return def;
         };
         resp.json().unwrap_or(def)
