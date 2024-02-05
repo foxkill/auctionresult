4d8c3af02b81a40723f0e25cdade83003ab34347
martinpfh <martin@pfh.de>
2024-02-05 16:57:31 +0100

    Add clap command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 //! # The Auction Result App
-// 
+//
+#[allow(unused)]
+use auctionresult::{Get, Latest, SecurityType, Treasury};
+use clap::Parser;
+use clap::Subcommand;
 
-use auctionresult::{Latest, Treasury, SecurityType};
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Retrieve the details of a treasury with the given cusip number.
+    #[command(arg_required_else_help = true)]
+    Get { cusip: String },
+    /// Retrieves the latest aution results.
+    #[command(arg_required_else_help = true)]
+    Latest {
+        #[arg(value_name = "type", long, value_enum, require_equals = true)]
+        /// The security type.
+        sectype: Option<SecurityType>,
+        #[arg(value_name = "days", long)]
+        /// The number of days we want to look back.
+        days: Option<usize>,
+    },
+}
 
+#[derive(Debug, Parser)]
+#[command(name = "ars")]
+#[command(bin_name = "ars")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
 fn main() {
-    // let l = Latest::new(TreasuryType::Bill, 0);
-    // let r: Vec<Treasury> = l.get();
+    // let AuctionResult::Get(args): AuctionResult = AuctionResult::parse();
 
+    println!("{:?}", Cli::parse());
     // println!("{:#?}", r);
 }
--- src/treasury/treasury_type.rs
+++ src/treasury/treasury_type.rs
@@ -1,7 +1,7 @@
 //! # The treasury type
 //!
 //! Represents the different types of treasuries.
-
+use clap::ValueEnum;
 use core::fmt;
 use serde::Deserialize;
 use std::str::FromStr;
@@ -9,7 +9,7 @@ use std::str::FromStr;
 #[derive(Debug)]
 pub struct ConvertError;
 
-#[derive(Debug, PartialEq, Deserialize, Default)]
+#[derive(Debug, PartialEq, Deserialize, Default, Clone, ValueEnum)]
 pub enum SecurityType {
     Bill,
     Note,
