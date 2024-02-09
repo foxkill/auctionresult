d15782a2761ad9c02a60ca050f7ec02f1584a204
martinpfh <martin@pfh.de>
2024-02-09 17:08:34 +0100

    Improve parse() function and add test.

--- src/tenor.rs
+++ src/tenor.rs
@@ -1,6 +1,5 @@
 //! # Parser for tenors.
 //!
-
 const RE: &str = r"^(?P<security>\d+)([-])?(?P<term>\w+)$";
 
 macro_rules! re {
@@ -10,48 +9,54 @@ macro_rules! re {
     }};
 }
 
-use std::num::ParseIntError;
 
 use crate::treasury::{AuctionResult, AuctionResultError};
+#[derive(Debug, PartialEq, Default)]
 pub struct Tenor {
     security: u32,
     term: String,
 }
 
+impl std::fmt::Display for Tenor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}-{}", self.security, self.term)
+    }
+}
+
 impl Tenor {
-    pub fn parse(s: &str) -> AuctionResult<Tenor> {
+    pub fn parse(s: &str) -> AuctionResult<Self> {
         // Guard
         let Some(captures) = re!(RE).captures(s) else {
-            return Err(AuctionResultError::Parse);
+            return Err(AuctionResultError::ParseCusip);
         };
 
         let security: &str = captures.name("security").map_or("", |m| m.as_str());
         let period: &str = captures.name("term").map_or("", |m| m.as_str());
 
         if security.is_empty() || period.is_empty() {
-            return Err(AuctionResultError::Parse);
+            return Err(AuctionResultError::ParseTenor);
         }
 
         let term = match period.to_lowercase().chars().next().unwrap_or(' ') {
             'y' => "Year",
             'w' => "Weeks",
-            'd' => "Days",        
-            _ => "",
+            'd' => "Day",        
+            _ => return Err(AuctionResultError::ParseTenor)
         };
 
-        if term.is_empty() {
-            return Err(AuctionResultError::Parse);
-        }
+        // Guard
+        let Ok(sec) = security.parse::<u32>() else {
+            return Err(AuctionResultError::ParseTenor);
+        };
 
-        let sec = security.parse::<u32>();
-        if sec.is_err() {
-            return Err(AuctionResultError::Parse);
-        }
-
-        Ok(Tenor {
+        Ok(Self {
             term: term.to_owned(),
-            security: sec.unwrap()
+            security: sec
         })
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.term.is_empty() && self.security == 0
     }
 }
 
@@ -67,5 +72,14 @@ mod tests {
         let tenor = result.unwrap();
         assert_eq!(tenor.security, 10);
         assert_eq!(tenor.term, "Year");
+        assert_eq!(tenor.to_string(), "10-Year");
+    }
+
+    #[test]
+    fn it_should_return_a_parse_error() {
+        let result = Tenor::parse("--Y");
+        
+        println!("{result:#?}");
+        assert!(result.is_err());
     }
 }
