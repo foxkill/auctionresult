8e73e4db59263fcbdb29f480d0a3a566e4fc5ad5
martinpfh <martin@pfh.de>
2024-02-09 17:07:55 +0100

    Rename TreasuryType to SecurityType

--- src/latest/auctioned.rs
+++ src/latest/auctioned.rs
@@ -1,6 +1,9 @@
 //! # Module for retrieving the lastest auction results.
 // #![allow(unused)]
-use crate::treasury::{load, treasury_type::SecurityType, AuctionResult, Treasuries, TreasuryAccess};
+use crate::{
+    tenor::Tenor,
+    treasury::{load, treasury_type::SecurityType, AuctionResult, Treasuries, TreasuryAccess},
+};
 
 #[cfg(not(test))]
 static AUCTIONED_URL: &str = "https://www.treasurydirect.gov/TA_WS/securities/auctioned";
@@ -10,7 +13,8 @@ static AUCTIONED_URL: &str = "/securities/auctioned";
 #[derive(Debug, Default, PartialEq)]
 pub struct Latest {
     days: usize,
-    treasury_type: SecurityType,
+    security_type: SecurityType,
+    tenor: Tenor,
     #[cfg(test)]
     host: String,
 }
@@ -21,17 +25,26 @@ impl TreasuryAccess<Treasuries> for Latest {
         let response = load(url)?;
 
         let treasuries: Treasuries = response.json()?;
-        Ok(treasuries)
+
+        Ok(if self.tenor.is_empty() {
+            treasuries
+        } else {
+            treasuries
+                .into_iter()
+                .filter(|t| *t.get_term() == self.tenor.to_string())
+                .collect::<Treasuries>()
+        })
     }
 
     fn url(&self) -> String {
         let mut url = String::from(AUCTIONED_URL);
-        if self.treasury_type != SecurityType::Null {
-            #[cfg(test)]
-            url.insert_str(0,&self.host);
 
+        #[cfg(test)]
+        url.insert_str(0, &self.host);
+
+        if self.security_type != SecurityType::Null {
             url.push_str("?type=");
-            url.push_str(&self.treasury_type.to_string());
+            url.push_str(&self.security_type.to_string());
             url.push_str("&days=");
             url.push_str(&self.days.to_string());
         } else {
@@ -43,17 +56,22 @@ impl TreasuryAccess<Treasuries> for Latest {
 }
 
 impl Latest {
-    pub fn new(treasury_type: SecurityType, days: usize) -> Self {
+    pub fn new(treasury_type: SecurityType, days: usize, tenor: Tenor) -> Self {
         Self {
             days: if days == 0 { 7 } else { days },
-            treasury_type,
+            security_type: treasury_type,
+            tenor,
             #[cfg(test)]
             host: "".to_owned(),
         }
     }
 
-    pub fn days(&self) -> usize {
+    pub fn get_days(&self) -> usize {
         self.days
+    }
+
+    pub fn get_security_type(&self) -> SecurityType {
+        self.security_type.to_owned()
     }
 }
 
@@ -68,20 +86,41 @@ mod tests {
     #[test]
     fn it_should_return_the_lastest_auctions() {
         let mut server = mockito::Server::new();
-        let mut latest = Latest::new(SecurityType::Bill, 8);
+        let mut latest = Latest::new(SecurityType::Bill, 8, Tenor::default());
 
         latest.host = server.url();
 
         server
             .mock("GET", AUCTIONED_URL)
             .match_query(Matcher::AllOf(vec![
-                Matcher::UrlEncoded("type".into(), SecurityType::Bill.to_string()),
-                Matcher::UrlEncoded("days".into(), "8".into()),
+                Matcher::UrlEncoded("type".into(), latest.get_security_type().to_string()),
+                Matcher::UrlEncoded("days".into(), latest.get_days().to_string()),
             ]))
             .with_body(api_multiple_items())
             .create();
 
         let response = latest.get().unwrap();
         assert_eq!(11, response.len());
+    }
+
+    #[test]
+    pub fn it_should_correctly_apply_tenor_filter() {
+        let sectype = SecurityType::Null;
+        let mut server = mockito::Server::new();
+        let mut latest = Latest::new(sectype.clone(), 8, Tenor::parse("2y").unwrap());
+
+        latest.host = server.url();
+
+        server
+            .mock("GET", AUCTIONED_URL)
+            .match_query(Matcher::UrlEncoded(
+                "days".into(),
+                "8".into(),
+            ))
+            .with_body(api_multiple_items())
+            .create();
+
+        let response = latest.get().unwrap();
+        assert_eq!(2, response.len());
     }
 }
