b9661a3349d82880b69d361115991c97ce28e7d7
martinpfh <martin@pfh.de>
2024-02-15 19:04:05 +0100

    Make the code more clean and unstandable.

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,18 @@ pub mod tenor;
 mod tests;
 pub mod treasury;
 
-#[cfg(feature = "quality")]
+// #[cfg(feature = "quality")]
 pub mod quality;
 
 // Re-exports - available modules.
 pub use get::Get;
 pub use latest::Latest;
 
+// Re-export Treasury Types
 pub use treasury::print::security_vprint;
 pub use treasury::SecurityType;
-pub use treasury::Treasury;
-pub use treasury::TreasuryAccess;
+// pub use self::treasury::Treasury;
+// pub use self::treasury::TreasuryAccess;
+// pub use self::treasury::AuctionResult;
+// pub use self::treasury::AuctionResultError;
+// pub use self::treasury::Treasuries;
--- src/quality.rs
+++ src/quality.rs
@@ -2,11 +2,11 @@
 //!
 //! measures the quality of an auction.
 
-#![allow(unused)]
-
+use crate::tenor::Tenor;
+use crate::treasury::Treasuries;
 use crate::{
-    treasury::{self, Treasuries},
-    TreasuryAccess,
+    treasury::{AuctionResult, AuctionResultError, Treasury, TreasuryAccess},
+    Get, Latest,
 };
 
 /// The number of auctions to consider.
@@ -15,14 +15,13 @@ const LAST_AUCTIONS: usize = 5;
 const BID_TO_COVER_RATIO_WEIGHT: f64 = 0.3;
 const PRIMARY_DEALER_ACCEPTED_WEIGHT: f64 = 0.2;
 const INDIRECT_BIDDER_WEIGHT: f64 = 0.1;
+#[allow(dead_code)]
 const WHEN_ISSUED_WEIGHT: f64 = 0.4;
 
 #[derive(Default, Debug)]
 pub struct Quality {
-    sum_bid_to_cover: f64,
-    sum_primary_dealers: f64,
-    sum_indirect_bidders: f64,
-    quality: f64,
+    cusip: String,
+    host: String,
 }
 
 // Auction bid-to-cover ratio = 2.3 (desired range: 2.0-3.0) - Score: 75
@@ -31,50 +30,111 @@ pub struct Quality {
 // 75 * 0.7 + 85 * 0.3 = 79.5
 
 impl Quality {
-    pub fn new() -> Self {
-        Self::default()
+    pub fn new(cusip: impl Into<String>) -> Self {
+        Self {
+            cusip: cusip.into(),
+            ..Self::default()
+        }
+    }
+
+    pub fn qget(&mut self) -> AuctionResult<f64> {
+        let mut get_command = Get::new(&self.cusip);
+
+        if cfg!(test) {
+            get_command.set_host(&self.host);
+        }
+
+        let treasuries = get_command.get();
+
+        let treasuries = treasuries?;
+
+        let Some(treasury) = treasuries.first() else {
+            return Err(AuctionResultError::NoTreasury);
+        };
+
+        let quality = self.calculate_quality(treasury, LAST_AUCTIONS)?;
+
+        Ok(quality)
+    }
+
+    fn set_host(&mut self, host: impl Into<String>) {
+        self.host = host.into();
     }
 
     /// Return the quality of the auction.
     //
     /// [`treasuries`]: The treasuries to consider.
     /// [`lookback_auctions`]: The _number_ of auctions to consider in the past.
-    pub fn auction_quality(&mut self, treasuries: &Treasuries, lookback_auctions: usize) -> f64 {
-        (
-            self.sum_bid_to_cover,
-            self.sum_primary_dealers,
-            self.sum_indirect_bidders,
-        ) = self.ratio_mean(treasuries, 5);
+    pub fn calculate_quality(
+        &self,
+        treasury: &Treasury,
+        lookback_auctions: usize,
+    ) -> AuctionResult<f64> {
+        // Get the term of the treasury.
+        let tenor = Tenor::parse(treasury.get_original_security_term())?;
+
+        // Get the security type.
+        let security_type = treasury.get_security_type();
+
+        // Create the lastest module and search for auctions that were happening
+        // before the given auction.
+        let mut latest = Latest::new(security_type, 0, tenor);
+
+        if cfg!(test) {
+            latest.set_host(&self.host);
+        }
+
+        let lastest_auctions = latest.get()?;
+
+        // Make sure we can look at the lastest X number of auctions.
+        if lastest_auctions.len() < LAST_AUCTIONS + 1 {
+            return Err(AuctionResultError::NoTreasury);
+        }
+
+        // Find the auction with given cusip.
+        let Some(pos) = lastest_auctions
+            .iter()
+            .position(|s| *s.cusip() == treasury.cusip()) else {
+            return Err(AuctionResultError::NoTreasury);
+        };
+
+        // Make sure we can look behind the lastest X number of auctions.
+        if pos + lookback_auctions > lastest_auctions.len() {
+            return Err(AuctionResultError::NoTreasury);
+        }
+
+        let treasuries = lastest_auctions
+            .iter()
+            .skip(pos)
+            .take(lookback_auctions + 1)
+            .collect::<Vec<&Treasury>>();
+
+        let (
+            sum_bid_to_cover,
+            sum_primary_dealers,
+            sum_indirect_bidders,
+        ) = self.ratio_mean(&treasuries, lookback_auctions);
 
         let treasury = treasuries.first().unwrap();
 
-        // println!(
-        //     "Treasury first: {:.2}->{:.2} {:.2}->{:.2} {:.2}->{:.2}",
-        //     treasury.get_bid_to_cover_ratio(),
-        //     self.sum_bid_to_cover,
-        //     treasury.get_percentage_debt_purchased_by_dealers(),
-        //     self.sum_primary_dealers,
-        //     treasury.get_percentage_debt_purchased_by_indirects(),
-        //     self.sum_indirect_bidders
-        // );
-
         let diff_bid_to_cover =
-            (treasury.get_bid_to_cover_ratio() - self.sum_bid_to_cover) * BID_TO_COVER_RATIO_WEIGHT;
+            (treasury.get_bid_to_cover_ratio() - sum_bid_to_cover) * BID_TO_COVER_RATIO_WEIGHT;
         let diff_primary_dealers = (treasury.get_percentage_debt_purchased_by_dealers()
-            - self.sum_primary_dealers)
+            - sum_primary_dealers)
             * -PRIMARY_DEALER_ACCEPTED_WEIGHT;
         let diff_indirect_bidders = (treasury.get_percentage_debt_purchased_by_indirects()
-            - self.sum_indirect_bidders)
+            - sum_indirect_bidders)
             * -INDIRECT_BIDDER_WEIGHT;
 
         // Round to 3 decimal places.
-        ((diff_bid_to_cover + diff_primary_dealers + diff_indirect_bidders) * 1000.0).round()
-            / 1000.0
+        let quality = ((diff_bid_to_cover + diff_primary_dealers + diff_indirect_bidders) * 1000.0).round() / 1000.0;
+
+        Ok(quality)
     }
 
     /// Calculate the mean of the _bid to cover_, _primary dealers_ and _indirect_bidders_ of
     /// the last [`last_auctions`] auctions.
-    fn ratio_mean(&self, treasuries: &Treasuries, last_auctions: usize) -> (f64, f64, f64) {
+    fn ratio_mean(&self, treasuries: &[&Treasury], last_auctions: usize) -> (f64, f64, f64) {
         if treasuries.len() < last_auctions + 1 {
             return (0.0, 0.0, 0.0);
         }
@@ -102,33 +162,50 @@ mod tests {
     use mockito::Matcher;
 
     use crate::{
-        latest::auctioned::AUCTIONED_URL, quality::LAST_AUCTIONS, tenor::Tenor,
-        tests::fixture::api_many_items, treasury::Treasuries, Latest, SecurityType, TreasuryAccess,
+        get::TREASURIES_URL,
+        latest::auctioned::AUCTIONED_URL,
+        tests::fixture::{api_30y_bond_item, api_many_items},
     };
 
-    use super::Quality;
+    use super::*;
+    const OUT_OF_BOUND_CUSIP: &str = "912810EY0";
+    const TEST_CUSIP: &str = "912810SH2";
 
     #[test]
     fn it_should_assess_the_quality_of_an_auction() {
         let mut server = mockito::Server::new();
-        let mut latest = Latest::new(SecurityType::Bill, 8, Tenor::default());
 
-        latest.set_host(server.url());
+        let mut quality = Quality::new(TEST_CUSIP);
+        quality.set_host(server.url());
+
+        println!("{:#?}", server.url());
+
+        server
+            .mock("GET", TREASURIES_URL)
+            .match_query(Matcher::AllOf(vec![
+                Matcher::UrlEncoded("cusip".into(), TEST_CUSIP.into()),
+                Matcher::UrlEncoded("format".into(), "json".into()),
+            ]))
+            .with_body(api_30y_bond_item())
+            .create();
 
         server
             .mock("GET", AUCTIONED_URL)
-            .match_query(Matcher::AllOf(vec![
-                Matcher::UrlEncoded("type".into(), latest.get_security_type().to_string()),
-                Matcher::UrlEncoded("days".into(), latest.get_days().to_string()),
-            ]))
+            .match_query(Matcher::AllOf(vec![Matcher::UrlEncoded(
+                "type".into(),
+                "Bond".into(),
+            )]))
             .with_body(api_many_items())
             .create();
 
-        let response: Treasuries = latest.get().unwrap();
+        // let response: Treasuries = latest.get().unwrap();
 
-        let mut quality = Quality::new();
-        let q = quality.auction_quality(&response, LAST_AUCTIONS);
+        let treasury = quality.qget();
+
+        println!("{:#?}", treasury);
+
+        // let q = quality.auction_quality(&response, LAST_AUCTIONS);
         // println!("Auction Quality is: {:#?}->{:.3}", quality, q);
-        assert_eq!(-0.545, q);
+        // assert_eq!(-0.545, q);
     }
 }
